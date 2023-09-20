60cbb2ef647efb5d02b10fd7ba68e6b46daf4db6
Clément <clement.roelens@hotmail.com>
2023-09-20 09:16:05 +0200

    finition

--- TP-listeDeCourses/main.js
+++ TP-listeDeCourses/main.js
@@ -1,89 +1,97 @@
-function createListItem(value,id) {
+function createListItem(value, id) {
     const li = document.createElement('li');
     const p = document.createElement('p');
     const div = document.createElement('div');
     const trash = document.createElement('i');
     const pencilSquare = document.createElement('i');
-    
+
     p.textContent = value;
     p.classList.add('ms-0');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between');
-    li.setAttribute('data-id',id);
+    li.setAttribute('data-id', id);
     div.classList.add('me-3');
     pencilSquare.classList.add('bi', 'bi-pencil-square', 'text-success', 'btn');
-    pencilSquare.setAttribute("data-id",id);
+    pencilSquare.setAttribute("data-id", id);
     trash.classList.add('bi', 'bi-trash', 'text-danger', 'btn');
-    trash.setAttribute("data-id",id);
-    
-    pencilSquare.addEventListener('click',e=>{
-        editItem(e.target.dataset.id);
+    trash.setAttribute("data-id", id);
+
+    pencilSquare.addEventListener('click', e => {
+        editItem(e.target.dataset.id, id);
     });
-    trash.addEventListener('click',e => {
+    trash.addEventListener('click', e => {
         eraseItem(e.target.dataset.id);
     });
-    
+
     div.appendChild(pencilSquare);
     div.appendChild(trash);
-    
+
     li.appendChild(p);
     li.appendChild(div);
-    
+
     ul.appendChild(li);
 }
 
-function editItem(value,id){
+function editItem(id) {
     const index = findItem(id);
     const li = document.querySelectorAll('li');
-    // shoppingList[index].name = value;
-    // localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-    // renderList();
+    for (const liElement of li) {
+        if (liElement.dataset.id == id) {
+            liElement.classList.add('display-6');
+            editMode = true;
+            editIndex = index;
+            input.value = shoppingList[index].name;
+            break;
+        }
+    }
 }
 
-function eraseItem(id){
-    shoppingList.splice(findItem(id),1);
+function eraseItem(id) {
+    shoppingList.splice(findItem(id), 1);
     localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
     renderList();
 }
 
-function findItem(id){
+function findItem(id) {
     const shoppingListIds = shoppingList.map(item => item.id);
     return shoppingListIds.indexOf(Number(id));
 }
 
-function addElementToList(){
+function addElementToList() {
     if (input.value !== "") {
+        const id = Date.now();
         const shoppingElement = {
-            id : count,
-            name : input.value
+            id: id,
+            name: input.value
         };
-        shoppingList.push(shoppingElement);
+        if (!editMode) {
+            shoppingList.push(shoppingElement);
+            createListItem(input.value, id);
+        } else {
+            shoppingList[editIndex].name = input.value;
+            editMode = false;
+            renderList();
+        }
         localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-        
-        createListItem(input.value,count);
-        
-        localStorage.setItem('count',++count);
         input.value = "";
     }
 }
 
-function renderList(){
+function renderList() {
     ul.innerHTML = '';
     shoppingList.forEach(storedElement => {
-        createListItem(storedElement.name,storedElement.id);
+        createListItem(storedElement.name, storedElement.id);
     })
 }
 
 
 const ul = document.querySelector('ul');
 let shoppingList = [];
-let count = 0;
+let editMode = false;
+let editIndex = 0;
 window.onload = () => {
     if (localStorage.getItem('shoppingList')) {
         shoppingList = JSON.parse(localStorage.getItem('shoppingList'));
         renderList();
-    }
-    if (localStorage.getItem('count')){
-        count = Number(localStorage.getItem('count'));
     }
 }
 
@@ -91,8 +99,8 @@ const add = document.querySelector('button');
 const input = document.querySelector('input');
 
 add.addEventListener('click', addElementToList);
-input.addEventListener('keydown',e=>{
-    if (e.key === "Enter"){
+input.addEventListener('keydown', e => {
+    if (e.key === "Enter") {
         addElementToList();
     }
 });
