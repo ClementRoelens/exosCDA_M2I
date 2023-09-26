f52897c9b229525aa0ac4980698c8c544f1e3e69
Clément <clement.roelens@hotmail.com>
2023-09-26 17:26:10 +0200

    Affichage role/occupation

--- exo-interface/main.js
+++ exo-interface/main.js
@@ -2,25 +2,27 @@ const personnes = [
     {
         nom: "Toto Dupont",
         age: 35,
-        occupation: "Facteur"
+        occupation: "facteur"
     },
     {
         nom: "Jeanne Doe",
         age: 25,
-        role: "Admin"
+        role: "admin"
     },
     {
         nom: "Michel Forever",
         age: 23,
-        occupation: "Lutteur"
+        occupation: "lutteur"
     },
     {
         nom: "Michael Flinch",
         age: 64,
-        role: "Gérant"
+        role: "gérant"
     }
 ];
 personnes.forEach((personne) => {
-    console.log(personne.nom + " a " + personne.age + " ans.");
+    let message = personne.nom + " a " + personne.age + " ans. ";
+    message += ('occupation' in personne) ? "Son occupation, c'est " + personne.occupation : "Son rôle, c'est " + personne.role;
+    console.log(message);
 });
 export {};
--- exo-interface/main.ts
+++ exo-interface/main.ts
@@ -7,25 +7,27 @@ const personnes:Personne[] = [
     {
         nom:"Toto Dupont",
         age:35,
-        occupation:"Facteur"
+        occupation:"facteur"
     },
     {
         nom : "Jeanne Doe",
         age:25,
-        role:"Admin"
+        role:"admin"
     },
     {
         nom:"Michel Forever",
         age:23,
-        occupation:"Lutteur"
+        occupation:"lutteur"
     },
     {
         nom:"Michael Flinch",
         age:64,
-        role:"Gérant"
+        role:"gérant"
     }
 ];
 
 personnes.forEach((personne:Personne) => {
-    console.log(personne.nom + " a " + personne.age + " ans.");
+    let message = personne.nom + " a " + personne.age + " ans. ";
+    message += ('occupation' in personne) ? "Son occupation, c'est " + personne.occupation : "Son rôle, c'est " + personne.role;
+    console.log(message);
 });
